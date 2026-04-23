eef9e9726043e0b48d710079e7e689a5c1232987
skobayashi36 <ngn2549516@stu.o-hara.ac.jp>
2026-04-23 14:26:47 +0900

    Merge branch 'master' of https://github.com/tama-git/Student_manager.git

--- src/main/java/scoremanager/SubjectUpdateAction.java
+++ src/main/java/scoremanager/SubjectUpdateAction.java
@@ -14,20 +14,24 @@ public class SubjectUpdateAction extends Action {
         HttpSession session = request.getSession();
         Teacher teacher = (Teacher) session.getAttribute("user");
         
-        // 1. パラメータ（科目コード）を取得
+     // 1. パラメータから科目コードを取得
         String cd = request.getParameter("cd");
 
-        // 2. SubjectDAOを使って既存の科目データを取得
+        // 2. SubjectDAOを使って既存の科目データを取得（引数は1つに変更）
         SubjectDAO sDao = new SubjectDAO();
-        // 現在のDAOの仕様（引数1つ）に合わせて取得
         Subject subject = sDao.get(cd);
 
-        // 3. JSPにデータを渡す
-        // 学校コードと科目コードは変更不可として表示するだけなので、
-        // subjectオブジェクトをそのまま渡せばOKです
+        // 3. 取得したsubjectから学校コードを取得する
+        // subjectがnullでないことを確認してから取得するとより安全です
+        if (subject != null && subject.getSchool() != null) {
+            String schoolCd = subject.getSchool().getCd();
+            // 必要であればここで schoolCd をログ出力したり、別の処理に使ったりできます
+        }
+
+        // 4. JSPにデータを渡す
         request.setAttribute("subject", subject);
 
-        // 4. 科目変更画面を表示
+        // 5. 科目変更画面を表示
         return "subject_update.jsp";
     }
 }

5c564eae799d2893f013a4165c38d7183d2de744
ketger <ngn2549574@stu.o-hara.ac.jp>
2026-04-24 10:01:16 +0900

    Merge branch 'master' of https://github.com/tama-git/Student_manager.git

--- src/main/java/scoremanager/SubjectUpdateAction.java
+++ src/main/java/scoremanager/SubjectUpdateAction.java
@@ -9,29 +9,29 @@ import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectUpdateAction extends Action {
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        HttpSession session = request.getSession();
-        Teacher teacher = (Teacher) session.getAttribute("user");
-        
-     // 1. パラメータから科目コードを取得
-        String cd = request.getParameter("cd");
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("user");
 
-        // 2. SubjectDAOを使って既存の科目データを取得（引数は1つに変更）
-        SubjectDAO sDao = new SubjectDAO();
-        Subject subject = sDao.get(cd);
+		// 1. パラメータから科目コードを取得
+		String cd = request.getParameter("cd");
 
-        // 3. 取得したsubjectから学校コードを取得する
-        // subjectがnullでないことを確認してから取得するとより安全です
-        if (subject != null && subject.getSchool() != null) {
-            String schoolCd = subject.getSchool().getCd();
-            // 必要であればここで schoolCd をログ出力したり、別の処理に使ったりできます
-        }
+		// 2. SubjectDAOを使って既存の科目データを取得（引数は1つに変更）
+		SubjectDAO sDao = new SubjectDAO();
+		Subject subject = sDao.get(cd);
 
-        // 4. JSPにデータを渡す
-        request.setAttribute("subject", subject);
+		// 3. 取得したsubjectから学校コードを取得する
+		// subjectがnullでないことを確認してから取得するとより安全です
+		if (subject != null && subject.getSchool() != null) {
+			String schoolCd = subject.getSchool().getCd();
+			// 必要であればここで schoolCd をログ出力したり、別の処理に使ったりできます
+		}
 
-        // 5. 科目変更画面を表示
-        return "subject_update.jsp";
-    }
+		// 4. JSPにデータを渡す
+		request.setAttribute("subject", subject);
+
+		// 5. 科目変更画面を表示
+		return "subject_update.jsp";
+	}
 }
